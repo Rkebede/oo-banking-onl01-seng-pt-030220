d44c3a123712de6718597f7de4082a192becc947
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-22 01:47:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,12 +16,10 @@ class Transfer
   def execute_transaction
     if @sender.balance < @amount || @status == "closed"
       return "Transaction rejected. Please check your account balance."
-    elsif @status != "closed" && @status == "pending" && @sender.balance > @amount
+    elsif @status == "pending" && @sender.balance > @amount
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    elsif @sender.balance < @amount
-      "Transaction rejected. Please check your account balance."
     end 
   end 
   
