ce59fcb86753d367db8f674f2c1e0049644f29e1
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-22 02:02:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @status == "pending" && @sender.balance >= @amount && @sender.status != "closed" && @receiver.status != "closed"
+    if @status == "pending" && @sender.balance >= @amount && valid?
        @sender.balance -= @amount
        @receiver.balance += @amount
        @status = "complete"
