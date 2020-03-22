a990d0f34632ec33a705340dc3d9b740ecfed258
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-22 02:05:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
        @receiver.balance += @amount
        @status = "complete"
      end 
-    elsif @sender.balance < @amount
+    else
         "Transaction rejected. Please check your account balance."
     end
   end 
